db3ca51cbfb71f0b5eca464200b1bf59e1ecbf1c
DevMain1953 <alexanderbrenenburgh@gmail.com>
2023-03-16 22:13:34 +0300

    Created new project

---
+++ KeyboardAndMemoryManagement/KeyboardManager.cs
@@ -0,0 +1,12 @@
+﻿using WinAPI;
+
+namespace KeyboardManagement
+{
+    public class KeyboardManager : WindowsAPI
+    {
+        public static bool IsKeyPushedDown(System.Int32 codeOfVirtualKey)
+        {
+            return (GetAsyncKeyState(codeOfVirtualKey) & 0x8000) != 0;
+        }
+    }
+}
