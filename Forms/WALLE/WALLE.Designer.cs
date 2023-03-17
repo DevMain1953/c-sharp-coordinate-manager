d0b6fb47f8f2f2d6f72bcf684eafa45e6a9c123d
DevMain1953 <alexanderbrenenburgh@gmail.com>
2023-03-17 08:48:40 +0300

    Added new form

---
+++ Forms/WALLE/WALLE.Designer.cs
@@ -0,0 +1,49 @@
+﻿
+namespace CoordinateManager.Forms.WALLE
+{
+    partial class WALLE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // WALLE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 450);
+            this.MaximumSize = new System.Drawing.Size(356, 489);
+            this.MinimumSize = new System.Drawing.Size(356, 489);
+            this.Name = "WALLE";
+            this.Text = "WALL-E";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
