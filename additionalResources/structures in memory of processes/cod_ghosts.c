5e6167679bb0c3d20dff2f0c1fdd4fbc4c6fad8e
KSergeyF <39850245+DevMain1953@users.noreply.github.com>
2025-10-20 06:13:10 -0900

    Create cod_ghosts.c

---
+++ additionalResources/structures in memory of processes/cod_ghosts.c
@@ -0,0 +1,11 @@
+struct player {
+	XCoordinate = player + 0x1C;
+	YCoordinate = player + 0x20;
+	zCoordinate = player + 0x24;
+
+	ZVector = player + 0x30;
+
+	noclip_mode = player + <find_out>;
+};
+
+HP = 0000000143C91600 + 0x17C;
