4b0029bd2f3b63666ad594ed9b96dfbc8e3a3d44
KSergeyF <39850245+DevMain1953@users.noreply.github.com>
2025-11-13 04:13:21 +0300

    Update cod_mw2.c

--- additionalResources/structures in memory of processes/cod_mw2.c
+++ additionalResources/structures in memory of processes/cod_mw2.c
@@ -11,4 +11,11 @@ struct player {
 	noclip_mode = player + 0xAD54;
 };
 
+Bytes of instruction that reads value in address of noclip:
+F6 80 54 AD 00 00 08
+
+Mask and bytes to search signature in OllyDbg:
+\xF6\x80\x54\xAD\x00\x00\x08 - bytes
+xx????x - mask
+
 structure_of_player = getAddressFrom(0x010A7190);
