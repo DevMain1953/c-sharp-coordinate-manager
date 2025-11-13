abf9df0b9bf6553d55da2e855569f9289fa3a1e2
KSergeyF <39850245+DevMain1953@users.noreply.github.com>
2025-11-13 04:16:12 +0300

    Update cod_ghosts.c

--- additionalResources/structures in memory of processes/cod_ghosts.c
+++ additionalResources/structures in memory of processes/cod_ghosts.c
@@ -8,4 +8,17 @@ struct player {
 	noclip_mode = player + <find_out>;
 };
 
+Code that creates script that enables and disables noclip mode without updating value in address of noclip mode:
+===============START
+[ENABLE]
+aobscanmodule(_no_clip_,iw6sp64_ship.exe,8B87????????????????????????8B3E??????A8047409)
+ _no_clip_:
+  db B8 01 00 00 00 90
+registersymbol(_no_clip_)
+[DISABLE]
+ _no_clip_:
+  db 8B 87 DC B6 00 00
+unregistersymbol(_no_clip_)
+================END
+
 HP = 0000000143C91600 + 0x17C;
